1297d6f977fb36d248306089b888cc934a60c06e
Amanpreet Kaur <744678@student.tor.loc>
2019-04-04 10:51:09 -0400

    diddd

--- file4.js
+++ file4.js
@@ -1,7 +1,7 @@
 var user ={
 first_name:"css",
 last_name:"HTML",
-age: 4,/*from w w  w. java  2 s   .c o m */
+age: 4, /*from w w  w. java  2 s   .c o m */
 wesite:"java2s.com"
 };
 for(key in user){
